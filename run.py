c38f1006184989cc4d9f22b8d846e0481682bea8
Agente1327 <al222310618@gmail.com>
2026-01-11 16:04:15 -0600

    Fix Flask factory for Render WSGI

--- run.py
+++ run.py
@@ -1,7 +1,4 @@
-from app import app
+from todor import create_app
 
-# Render y Gunicorn usan esta variable
-application = app
+application = create_app()
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
